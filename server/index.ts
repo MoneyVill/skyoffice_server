da2c0d0e8b49c903d9747d00f3f35d745fe4b9ea
Debian <eun0571@gmail.com>
2024-11-19 16:50:13 +0000

    update //배포용 or //로컬용

--- server/index.ts
+++ server/index.ts
@@ -11,12 +11,17 @@ import { SkyOffice } from './rooms/SkyOffice'
 const port = Number(process.env.PORT || 1234)
 const app = express()
 
-// app.use(cors({}));
-app.use(cors({
-  origin: '*'
-}));
+//배포용
+app.use(cors());
+//로컬용
+// app.use(cors({
+//   origin: '*'
+// }));
 
-app.use(express.json())
+//배포용
+app.use('/client',express.json())
+//로컬용
+// app.use(express.json())
 // app.use(express.static('dist'))
 
 const server = http.createServer(app)
@@ -24,9 +29,10 @@ const gameServer = new Server({
   server,
 })
 
-app.get('/health', (req, res) => {
-  res.status(200).send('Server is healthy');
-});
+//로컬용
+// app.get('/health', (req, res) => {
+//   res.status(200).send('Server is healthy');
+// });
 
 // register room handlers
 gameServer.define(RoomType.LOBBY, LobbyRoom)
@@ -53,6 +59,10 @@ app.use('/colyseus', monitor())
 // gameServer.listen(port);
 // console.log(`Listening on ws://localhost:${port}`);
 
-gameServer.listen(port, '0.0.0.0');
-console.log(`Listening on https://0.0.0.0:${port}`);
-console.log(`Server accessible via https://13.124.171.11:${port}`);
+//배포용
+gameServer.listen(port)
+console.log(`Listening on ws://localhost:${port}`)
+//로컬용
+// gameServer.listen(port, '0.0.0.0');
+// console.log(`Listening on https://0.0.0.0:${port}`);
+// console.log(`Server accessible via https://13.124.171.11:${port}`);
