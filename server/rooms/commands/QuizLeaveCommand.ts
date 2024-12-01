8c0e59c39f7da2575dcf67b6c5f4c3cdcecb3bd6
박성현 <110206842+shPark95@users.noreply.github.com>
2024-12-02 01:56:43 +0900

    modify: quizevent

--- server/rooms/commands/QuizLeaveCommand.ts
+++ server/rooms/commands/QuizLeaveCommand.ts
@@ -3,6 +3,7 @@
 import { Command } from '@colyseus/command';
 import { Client } from 'colyseus';
 import { IOfficeState } from '../../../types/IOfficeState';
+import { Message } from '../../../types/Messages'
 
 type Payload = {
   client: Client;
@@ -12,7 +13,9 @@ export default class QuizLeaveCommand extends Command<IOfficeState, Payload> {
   execute({ client }: Payload) {
     // 퀴즈에서 해당 사용자를 제거하는 로직을 구현합니다.
     // 예를 들어, 퀴즈 참가자 목록에서 제거하거나 상태를 업데이트합니다.
-
+  
+    client.send(Message.LEFT_QUIZ, {
+    })
     // 필요하다면 다른 클라이언트에게 해당 사용자가 퀴즈에서 나갔음을 알릴 수 있습니다.
     // this.room.broadcast(Message.PLAYER_LEFT_QUIZ, { clientId: client.sessionId });
   }
